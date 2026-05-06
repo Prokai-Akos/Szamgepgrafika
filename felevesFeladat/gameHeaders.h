82007f4f4b9020666b466f0923501e8fcdacc0a0
ProkaiAkos <prokaiakos2004@gmail.com>
2026-05-06 16:39:20 +0200

    Lots of work made -textured map, map grid defined in map.h -texture loaders, camera and particle structs, fog and particle functions -can work around a(for now) flat map, with rain particles implemented -partial helpmenu implemented by pressing F1

---
+++ felevesFeladat/gameHeaders.h
@@ -0,0 +1,225 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_opengl.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * Kamera kezeléshez struct
+ */
+typedef struct {
+    float x, y, z;
+    float yaw;
+    float pitch;
+} Camera;
+
+/**
+ * Particle kezelő struct 
+ * */
+typedef struct {
+    float x, y, z;
+    float vy;//speed
+    float life;//lifetime
+} Particle;
+
+#define maxParticle 600
+static Particle particles[maxParticle];
+
+
+/**
+ * Taken from M_PI definition of math.h, using M_PI works
+ * but the file has an error since it only gets replaced at compile time,
+ * i didnt want to see the error so i just did it this way 
+ */
+static double pi = 3.14159265358979323846;
+
+
+/**
+ * texture loader, static so if i add it to more files the compiler wont kill itself
+ */
+static GLuint loadTexture(const char* filename) {
+    SDL_Surface* surface = IMG_Load(filename);
+    if (!surface) {
+        printf("Failed to load image: %s\n", IMG_GetError());
+        return 0;
+    }
+
+    GLuint textureID;
+    glGenTextures(1, &textureID);
+    glBindTexture(GL_TEXTURE_2D, textureID);
+
+    // Set texture parameters (Wrapping and Filtering)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+    // Upload data to GPU
+    int mode = (surface->format->BytesPerPixel == 4) ? GL_RGBA : GL_RGB;
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);// Force OpenGL to read pixels byte-by-byte, ignoring 4-byte row alignment
+    glTexImage2D(GL_TEXTURE_2D, 0, mode, surface->w, surface->h, 0, mode, GL_UNSIGNED_BYTE, surface->pixels);
+
+    SDL_FreeSurface(surface);
+    return textureID;
+}
+
+static GLuint textToTexture(TTF_Font* font, const char* text) {
+    if (!font || !text || text[0] == '\0') return 0;
+
+    SDL_Color white = {255, 255, 255, 255};
+    SDL_Surface* initialSurf = TTF_RenderUTF8_Blended(font, text, white);
+    if (!initialSurf) return 0;
+
+    SDL_Surface* surf = SDL_ConvertSurfaceFormat(initialSurf, SDL_PIXELFORMAT_ABGR8888, 0);
+    SDL_FreeSurface(initialSurf);
+
+    GLuint textureID;
+    glGenTextures(1, &textureID);
+    glBindTexture(GL_TEXTURE_2D, textureID);
+
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, surf->w, surf->h, 0, GL_RGBA, GL_UNSIGNED_BYTE, surf->pixels);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    SDL_FreeSurface(surf);
+    return textureID;
+}
+
+static void drawHelpMenu(TTF_Font* font) {
+    // 1. Switch to 2D
+    glMatrixMode(GL_PROJECTION);
+    glPushMatrix();
+    glLoadIdentity();
+    gluOrtho2D(0, 800, 600, 0);
+    glMatrixMode(GL_MODELVIEW);
+    glPushMatrix();
+    glLoadIdentity();
+
+    glDisable(GL_DEPTH_TEST);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    // 2. Draw Dark Overlay
+    glDisable(GL_TEXTURE_2D);
+    glColor4f(0.0f, 0.0f, 0.0f, 0.8f);
+    glBegin(GL_QUADS);
+        glVertex2f(50, 50); glVertex2f(750, 50);
+        glVertex2f(750, 550); glVertex2f(50, 550);
+    glEnd();
+
+    // 3. Render Lines
+    glEnable(GL_TEXTURE_2D);
+    glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+    
+    const char* lines[] = {
+        "HASZNALATI UTMUTATO",
+        "-------------------",
+        "- W,A,S,D: Mozgas",
+        "- L-CTRL: Gugolas",
+        "- EGER: Nezegetes",
+        "- F1: Sugo bezarasa",
+        "- ESC: Kilepes"
+    };
+
+    int startY = 100;
+    for (int i = 0; i < 7; i++) {
+        GLuint tex = textToTexture(font, lines[i]);
+        
+        // Use a standard height for lines, or get w/h from surface if preferred
+        glBindTexture(GL_TEXTURE_2D, tex);
+        glBegin(GL_QUADS);
+            glTexCoord2f(0, 0); glVertex2f(100, startY + (i * 40));
+            glTexCoord2f(1, 0); glVertex2f(400, startY + (i * 40));
+            glTexCoord2f(1, 1); glVertex2f(400, startY + 35 + (i * 40));
+            glTexCoord2f(0, 1); glVertex2f(100, startY + 35 + (i * 40));
+        glEnd();
+        
+        glDeleteTextures(1, &tex);
+    }
+
+    // 4. Restore 3D
+    glDisable(GL_BLEND);
+    glEnable(GL_DEPTH_TEST);
+    glPopMatrix();
+    glMatrixMode(GL_PROJECTION);
+    glPopMatrix();
+    glMatrixMode(GL_MODELVIEW);
+}
+
+/**
+ * Blindness effect-nek felhasználható lesz majd, esetleg a kuplungért be kell menni
+ * egy házba ahol sötét lesz és ott megkeresni sötétben?
+ */
+static void enableFog(){
+    GLfloat fogColor[4] = {0.5f,0.5f,0.5f,0.5f};
+    glClearColor(0.5f,0.5f,0.5f,0.5f);
+    glEnable(GL_FOG);
+    glFogf(GL_FOG_MODE, GL_LINEAR);
+    glFogf(GL_FOG_START, 1.0f);
+    glFogfv(GL_FOG_COLOR, fogColor);
+    glFogf(GL_FOG_END, 5.0f);
+}
+
+/**
+ * Initializes particles
+ */
+static void initParticles(){
+    for (int i = 0; i < maxParticle; i++){
+        particles[i].x = (rand() % 20) - 10;
+        particles[i].y = (rand() % 10);
+        particles[i].z = (rand() % 20) - 10;
+        particles[i].vy = -0.09f; // Lefelé eső (pl. eső vagy hó)
+        particles[i].life = (float)rand() / RAND_MAX;
+    }
+};
+
+/**
+ * Displayes the particles, takes a cam so particles are generated even when the camera moves
+ * and it always generates around the camera even when it moves
+ */
+static void displayParticles(Camera cam){
+    glDisable(GL_TEXTURE_2D);
+    glPointSize(5.0f);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glDepthMask(GL_FALSE); // A részecskék ne takarják ki egymást a mélységtesztben
+
+    glBegin(GL_POINTS);
+    for(int i = 0; i < maxParticle; i++) {
+        if (particles[i].life > 0) {
+            particles[i].y += particles[i].vy;
+            particles[i].life -= 0.005f;
+
+            //glColor4f(1.0f, 1.0f, 1.0f, particles[i].life);//szürke köd particle?
+            glColor4f(0.2f, 0.4f, 1.0f, particles[i].life);//kék esőhöz
+            glVertex3f(particles[i].x, particles[i].y,  particles[i].z);
+
+            // Ha meghalt vagy leesett, a KAMERA KÖRÉ tegyük vissza
+            if(particles[i].life <= 0 || particles[i].y < -1.0f) {
+                particles[i].life = 1.0f;
+                particles[i].y = cam.y + 5.0f; // Magasról essen
+                
+                // A kamera X és Z koordinátája köré rakjuk +- 20 egységgel
+                particles[i].x = cam.x + (rand() % 40) - 20;
+                particles[i].z = cam.z + (rand() % 40) - 20;
+            }
+        }
+    }
+    glEnd();
+
+    glDepthMask(GL_TRUE);
+    glDisable(GL_BLEND);
+    glEnable(GL_TEXTURE_2D);
+    glColor3f(1.0f, 1.0f, 1.0f);
+}
+
+#endif
--- felevesFeladat/main.c
+++ felevesFeladat/main.c
@@ -1,89 +1,184 @@
-#include <GL/gl.h>
-#include <GL/glu.h>
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_opengl.h>
-#include <stdbool.h>
-#include <stdio.h>
+#include "gameHeaders.h"
+#include "map.h"
 
-//Kamera állapota
-typedef struct {
-    float x, y, z;
-    float yaw;
-} Camera;
-
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0){
         printf("[ERROR] SDL initialization error: %s\n", SDL_GetError());
         return -1;
     }
 
-    SDL_Window* window = SDL_CreateWindow(
-        "3D Floating Camera",
+    if (TTF_Init() == -1) printf("TTF_Init error: %s\n", TTF_GetError());
+
+    TTF_Font* font = TTF_OpenFont("assets/computer-font/Computer-z2aL.ttf", 24); // Make sure you have a .ttf file!
+    if (!font) printf("Font load error: %s\n", TTF_GetError());
+
+    SDL_Window *window = SDL_CreateWindow(
+        "Leendő játék",
         SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
         800, 600,
         SDL_WINDOW_OPENGL);
 
     SDL_GLContext gl_context = SDL_GL_CreateContext(window);
+    SDL_GL_SetSwapInterval(1); // 1 = Enable VSync, 0 = Disable
+    SDL_SetRelativeMouseMode(SDL_TRUE);
 
     // 3D perspektíva
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     // FOV, Aspect Ratio, Near Clip, Far Clip
     gluPerspective(45.0, 800.0 / 600.0, 0.1, 100.0);
-    
+
     glMatrixMode(GL_MODELVIEW);
     glEnable(GL_DEPTH_TEST);
+    glEnable(GL_TEXTURE_2D);
 
-    Camera cam = {0.0f, 0.0f, 5.0f, 0.0f}; // 5 egységgel távolabbi start
+    //texture loading
+    GLuint grass = loadTexture("assets/grass.jpg");
+    GLuint dirt = loadTexture("assets/dirt.jpg");
+    //enableFog();
+    glClearColor(0.5f, 0.8f, 1.0f, 1.0f); //clear sky
+    //glClearColor(0.5f,0.5f,0.5f,0.5f);//foggy sky
+    initParticles();
+
+    float tileSize = 1.0f;
+    float centerX = (WIDTH * tileSize) / 2.0f;
+    float centerZ = (HEIGHT * tileSize) / 2.0f;
+    Camera cam = {centerX, 0.0f, centerZ, 0.0f, 0.0f}; // map közepi start
     bool need_run = true;
     SDL_Event event;
+    const Uint8 *state = SDL_GetKeyboardState(NULL);//állandó gomblenyomásos mozgásű
+    bool showHelp = false;
+    float lightLevel = 0.5f; //0.0-1.0 között legyen
 
-    while (need_run) {
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) need_run = false;
-            
-            if (event.type == SDL_KEYDOWN) {
-                switch (event.key.keysym.sym) {
-                    case SDLK_ESCAPE: need_run = false; break;
-                    // Move
-                    case SDLK_w: cam.z -= 0.2f; break;
-                    case SDLK_s: cam.z += 0.2f; break;
-                    case SDLK_a: cam.x -= 0.2f; break;
-                    case SDLK_d: cam.x += 0.2f; break;
-                    // Look
-                    case SDLK_q: cam.yaw -= 2.0f; break;
-                    case SDLK_e: cam.yaw += 2.0f; break;
+
+    while (need_run){
+        float rad = cam.yaw * (pi / 180.0f);
+        float speed = 0.05f; //"walking" speed
+
+        float normalHeight = 0.0f;  // alap eye-level
+        float crouchHeight = -0.6f; // gugolós eye-level
+        float bobbingHeight = 0.1f; // view bobbingos
+
+        if ((state[SDL_SCANCODE_W] || state[SDL_SCANCODE_S]) && (state[SDL_SCANCODE_A] || state[SDL_SCANCODE_D])) 
+            speed = speed * 0.7071f; // 1 / sqrt(2)
+        
+
+        while (SDL_PollEvent(&event)){
+            if (event.type == SDL_QUIT)
+                need_run = false;
+
+            if (event.type == SDL_KEYDOWN){
+                switch (event.key.keysym.sym){
+                case SDLK_ESCAPE:
+                    need_run = false;
+                    break;
+                case SDLK_F1:
+                    showHelp = !showHelp;
+                    if (showHelp) SDL_SetRelativeMouseMode(SDL_FALSE); 
+                    else SDL_SetRelativeMouseMode(SDL_TRUE);
+                    break;
+                case SDLK_PLUS:
+                case SDLK_KP_PLUS:
+                        lightLevel += 0.05f;
+                    if (lightLevel > 1.0f) lightLevel = 1.0f;
+                    break;
+                case SDLK_MINUS:
+                case SDLK_KP_MINUS:
+                    lightLevel -= 0.05f;
+                    if(lightLevel < 0) lightLevel = 0;
+                    break;
                 }
+            }
+
+            if (event.type == SDL_MOUSEMOTION){
+                float sensitivity = 0.2f;
+                cam.yaw += event.motion.xrel * sensitivity;
+                cam.pitch += event.motion.yrel * sensitivity;
+
+                if (cam.pitch > 89.0f)
+                    cam.pitch = 89.0f;
+                if (cam.pitch < -89.0f)
+                   cam.pitch = -89.0f;
             }
         }
 
-        // Scene render
-        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        
-        glLoadIdentity();
+        //gugolás implementáció
+        if (state[SDL_SCANCODE_LCTRL]) cam.y = crouchHeight; 
+        else cam.y = normalHeight;
 
-        // kamera transzformációk
-        glRotatef(cam.yaw, 0, 1, 0);   
-        glTranslatef(-cam.x, -cam.y, -cam.z);
-
-        // padló
-        glBegin(GL_LINES);
-        glColor3f(0.5f, 0.5f, 0.5f);
-        for(float i = -10; i <= 10; i += 1.0f) {
-            glVertex3f(i, -1.0f, -10.0f); glVertex3f(i, -1.0f, 10.0f);
-            glVertex3f(-10.0f, -1.0f, i); glVertex3f(10.0f, -1.0f, i);
+        //wasd mozgás
+        if (state[SDL_SCANCODE_W]){
+            cam.x += sinf(rad) * speed;
+            cam.z -= cosf(rad) * speed;
         }
-        glEnd();
+        if (state[SDL_SCANCODE_S]){
+            cam.x -= sinf(rad) * speed;
+            cam.z += cosf(rad) * speed;
+        }
+        if (state[SDL_SCANCODE_A]){
+            cam.x -= cosf(rad) * speed;
+            cam.z -= sinf(rad) * speed;
+        }
+        if (state[SDL_SCANCODE_D]){
+            cam.x += cosf(rad) * speed;
+            cam.z += sinf(rad) * speed;
+        }
+
+        // Scene render
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glLoadIdentity();
+        
+        // kamera transzformációk
+        glRotatef(cam.pitch, 1.0f, 0.0f, 0.0f);
+        glRotatef(cam.yaw, 0.0f, 1.0f, 0.0f);
+        glTranslatef(0.0f, -cam.y, 0.0f); // Csak a magasság eltolása
+        glTranslatef(-cam.x, 0.0f, -cam.z); // Csak a síkbeli mozgás
+
+        glBindTexture(GL_TEXTURE_2D, grass);
+        glColor3f(lightLevel, lightLevel, lightLevel);//adjust color based on lightlevel
+
+        for (int i = 0; i < WIDTH; i++) {
+            for (int j = 0; j < HEIGHT; j++) {
+                
+                // Pick the texture based on the array value
+                if (tileMap[i][j] == 0)      glBindTexture(GL_TEXTURE_2D, grass);
+                else if (tileMap[i][j] == 1) glBindTexture(GL_TEXTURE_2D, dirt);
+
+                glBegin(GL_QUADS);
+                    glTexCoord2f(0.0f, 0.0f); glVertex3f(i * tileSize, -1.0f, j * tileSize);
+                    glTexCoord2f(1.0f, 0.0f); glVertex3f((i+1) * tileSize, -1.0f, j * tileSize);
+                    glTexCoord2f(1.0f, 1.0f); glVertex3f((i+1) * tileSize, -1.0f, (j+1) * tileSize);
+                    glTexCoord2f(0.0f, 1.0f); glVertex3f(i * tileSize, -1.0f, (j+1) * tileSize);
+                glEnd();
+            }
+        }
+
+        glBindTexture(GL_TEXTURE_2D, 0); // Unbind so the triangle doesn't get textured
+
+        displayParticles(cam);
+
 
         // példa háromszög
         glBegin(GL_TRIANGLES);
-            glColor3f(1, 0, 0); glVertex3f(-1, -1, 0);
-            glColor3f(0, 1, 0); glVertex3f( 1, -1, 0);
-            glColor3f(0, 0, 1); glVertex3f( 0,  1, 0);
+        glColor3f(1, 0, 0);
+        glVertex3f(centerX -1.0f, -1.0f, centerZ - 0.5f);
+        glColor3f(0, 1, 0);
+        glVertex3f(centerX + 1.0f, -1.5f, centerZ- 0.5f);
+        glColor3f(0, 0, 1);
+        glVertex3f(centerX, 1.0f, centerZ - 0.5f);
         glEnd();
 
+        glDisable(GL_BLEND);
+        glEnable(GL_DEPTH_TEST);
+        
+        glPopMatrix();
+        glMatrixMode(GL_PROJECTION);
+        glPopMatrix();
+        glMatrixMode(GL_MODELVIEW);
+
+        if(showHelp) drawHelpMenu(font);
         SDL_GL_SwapWindow(window);
     }
 
